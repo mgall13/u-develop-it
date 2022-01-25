0a125044562d632ab6cd24b47882f9e2353b6d2d
Kyro <mariogallardo1331@yahoo.com>
2022-01-25 11:27:33 -0600

    incorporating SQL into node.js

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -6,6 +7,21 @@ const app = express();
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Connecting to MySQL Database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: 'Loulousr2010!',
+        database: 'election'
+    },
+    console.log('Connected to the election database!')
+);
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 // Default response for any other request (Not Found)
 // ALWAYS MAKE SURE THIS IS THE LAST ROUTE
@@ -15,4 +31,4 @@ app.use((req, res) => {
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
-})
+});
